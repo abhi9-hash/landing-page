88b82915c9ef5f93fb892d1a2e947d6c7f281495
abhnv_pthk <coolabhinav57@gmail.com>
2021-03-25 03:38:08 +0530

    added new sections

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,17 @@ import axios from 'axios';
 import bgelement1 from './img/bg-element1.svg';
 import bgelement2 from './img/bg-element4.svg';
 import bgelement3 from './img/bg-element6.svg';
+import bgelement4 from './img/bg-element2.svg';
+import bgelement5 from './img/bg-element3.svg';
+import bgelement6 from './img/bg-element5.svg';
+import mainbg from './img/main-bg.svg';
 import who1 from './img/who-1.svg';
 import who2 from './img/who-2.svg';
 import who3 from './img/who-3.svg';
+import howto1 from './img/howto-1.svg';
+import howto2 from './img/howto-2.svg';
+import howto3 from './img/howto-3.svg';
+import howto4 from './img/howto-4.svg';
 import 'semantic-ui-css/semantic.min.css';
 import {  Form, Container, Header} from 'semantic-ui-react';
 import {Button,Modal, ModalHeader, ModalFooter, ModalBody, ModalTitle} from 'react-bootstrap';
@@ -32,7 +40,6 @@ const useScript = url => {
   }, [url]);
 };
 
-
 function App() {
   const [data, setdata]=useState({name:'', number:''
 })
@@ -53,16 +60,77 @@ function App() {
 
   return (
     <div className="App">
-      <div className="header1">
-        <img className="img1" src={bgelement1}></img>
-        <img className="img2" src={bgelement2}></img>
-        <img className="img3" src={bgelement3}></img>
+      <div className="heading1"><p>India's Most Promising Job Site</p></div>
+      <div className="heading2"><p><b>Simple 15 Days To Get Your First Job</b></p></div>
+      <div className="headertop">
+        <div className="number"><input className="number-in" placeholder="Enter Your Number to Get Started"></input><button className="bt-in">Go</button></div>
         </div>
-   
+        <div className="heading3"><p>How To Get Jobs In 4 Easy Steps</p></div>
+
+        <div className="grid1">
+        <div className="grid11"><div className="div"><div style={{height:'50px',width:'50px',borderRadius:'50%',backgroundColor:'white', display:'flex',justifyContent:'center', alignItems:'center',position:'absolute', marginLeft:'200px', marginBottom:'180px'}}><b style={{fontSize:'xx-large'}}>1</b></div><img className="howto1" src={howto1}></img></div><div><p><b>Enroll in the Program</b></p></div></div>
+        <div className="grid22" ><div className="div"><div style={{height:'50px',width:'50px',borderRadius:'50%',backgroundColor:'white', display:'flex',justifyContent:'center', alignItems:'center',position:'absolute', marginLeft:'200px', marginBottom:'180px'}}><b style={{fontSize:'xx-large'}}>2</b></div><img className="howto2" src={howto2}></img></div><div><b>Engage with Our Experts</b></div></div>
+        <div className="grid33"><div className="div"><div style={{height:'50px',width:'50px',borderRadius:'50%',backgroundColor:'white', display:'flex',justifyContent:'center', alignItems:'center',position:'absolute', marginLeft:'200px', marginBottom:'180px'}}><b style={{fontSize:'xx-large'}}>3</b></div><img className="howto3" src={howto3}></img></div><div><b>Excel in our Assessments</b></div></div>
+        <div className="grid44"><div className="div"><div style={{height:'50px',width:'50px',borderRadius:'50%',backgroundColor:'white', display:'flex',justifyContent:'center', alignItems:'center',position:'absolute', marginLeft:'200px', marginBottom:'180px'}}><b style={{fontSize:'xx-large'}}>4</b></div><img className="howto4" src={howto4}></img></div><div><b>Earn the Job</b></div></div>
+        </div>
+        <div>
+          
+        <div className="heading4"><p> Get Your Next Job Now! </p></div>
+        <div>
+          <img  
+          style={{maxHeight:'300px',maxWidth:'200px',zIndex:'-1',position:'absolute', marginTop:'-90px'}} 
+          src={bgelement1}></img>
+          <div style={{marginTop:'40px'}} className="heading5"><p style={{fontSize:'50px'}}><b> How we Make a Difference </b></p></div>
+          
+          <div className="header"><img 
+          src={bgelement4}></img>
+          <img 
+          style={{marginTop:'-100px'}} 
+          src={bgelement4}></img>
+          </div>
+          </div>
+        </div>
+
+        <div>
+          <div>
+          <img src={bgelement5} style={{position:'absolute',marginLeft:'10vw',marginTop:'300px',width:'20vw', zIndex:'-1'}}></img>
+          <img src={bgelement2} style={{position:'absolute',marginBottom:'200px',width:'20vw', zIndex:'-1'}}></img>
+          </div>
+        <div className="grid2">
+        <div className="grid-1" ><b style={{fontSize:'150px',color:'lightgray',zIndex:'-1',borderRadius:'50%', display:'flex',justifyContent:'center', alignItems:'center',position:'absolute', marginLeft:'200px', marginBottom:'-190px'}}>01</b><div><p>Guaranteed Job Placement</p></div></div>
+        <div className="grid-2" ><b style={{fontSize:'150px',color:'lightgray',zIndex:'-1',borderRadius:'50%', display:'flex',justifyContent:'center', alignItems:'center',position:'absolute', marginLeft:'200px', marginBottom:'-190px'}}>02</b><div><p>Expert Curated Programs</p></div></div>
+        <div className="grid-3"><b style={{fontSize:'150px',color:'lightgray',zIndex:'-1',borderRadius:'50%', display:'flex',justifyContent:'center', alignItems:'center',position:'absolute', marginLeft:'200px', marginBottom:'-190px'}}>03</b><div><p>Free Interview Preparation Toolkit</p></div></div>
+        <div className="grid-4" ><b style={{fontSize:'150px',color:'lightgray',zIndex:'-1',borderRadius:'50%', display:'flex',justifyContent:'center', alignItems:'center',position:'absolute', marginLeft:'200px', marginBottom:'-190px'}}>04</b><div><p>Assured Moneyback On Promise Break</p></div></div>
+        <div className="grid-5" ><b style={{fontSize:'150px',color:'lightgray',zIndex:'-1',borderRadius:'50%', display:'flex',justifyContent:'center', alignItems:'center',position:'absolute', marginLeft:'200px', marginBottom:'-190px'}}>05</b><div><p>Lifetime Partnership</p></div></div>
+        <div className="grid-6"><b style={{fontSize:'150px',color:'lightgray',zIndex:'-1',borderRadius:'50%', display:'flex',justifyContent:'center', alignItems:'center',position:'absolute', marginLeft:'200px', marginBottom:'-190px'}}>06</b><div><p>One to One Assistance</p></div></div>
+        </div>
+        </div>
+        <div className="header" ><img style={{marginTop:'-100px',height:'300px'}} 
+          src={bgelement4}></img>
+          </div>
+        <div className="heading5" style={{width:'100vw'}}>
+        <img style={{width:'20vw', zIndex:'-1',position:'absolute',marginLeft:'-40vw'}} src={bgelement6}>
+          </img>
+          
+          <p style={{fontSize:'50px',marginTop:'-100px'}}> Our Plans </p>
+        
+        <img style={{width:'40vw', zIndex:'-1',position:'absolute'}} src={bgelement5}>
+          </img>
+          
+        <img style={{width:'30vw', zIndex:'-1',position:'absolute',marginLeft:'20vw'}} src={bgelement4}></img>
+        
+        <img style={{width:'20vw', zIndex:'-1',position:'absolute'}} src={bgelement1}></img></div>
+        
+        <div className="grid3">
+        <div className="grid1-3"><div><p>Anyone who is looking for a job but not sure which job role to take. Personnels actively looking for job role</p><span>Free</span></div></div>
+        <div className="grid2-3" ><div><p>Students about to start their professional journey. Personnels actively looking for job role</p><span>Free</span></div></div>
+        <div className="grid3-3"><div><p>Personnels actively looking for job role. Personnels actively looking for job role</p><span>Free</span></div></div>
+        </div>
+     
      <div className="button"> <button onClick={handleShow} className="bt"><p>Register</p></button>
      <Modal  show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Register</Modal.Title>
+          <Modal.Title ><p Style={{alignText:'center'}}>Register</p></Modal.Title>
         </Modal.Header>
         <Modal.Body> 
           {     <Container fluid className="container">
@@ -93,16 +161,34 @@ function App() {
         </Modal.Footer>
       </Modal>
      </div>
-      <div className="heading"><p>Who Is This Course For?</p></div>
-      <div className="img-bg">
-        
-        
-        
-      </div>
-      <div className="grid">
-        <div className="grid1"><img className="img4" src={who1}></img><div><p>Anyone who is looking for a job but not sure which job role to take.</p></div></div>
-        <div className="grid2" ><img className="img5" src={who2}></img><div><p>Students about to start their professional journey</p></div></div>
-        <div className="grid3"><img className="img6" src={who3}></img><div><p>Personnels actively looking for job role</p></div></div>
+
+     <div className="heading6"><p style={{fontSize:'50px', marginTop:'30vh'}}>BDE Course Details</p></div>
+       
+        <div className="grid4" style={{zIndex:'1'}} >
+        <div className="grid1-4"><div><p>Anyone who is looking for a job but not sure which job role to take. Personnels actively looking for job role</p><span><ul><li>🟦</li><li>🟦</li><li>🟦</li><li>🟦</li></ul></span></div></div>
+        <div className="grid2-4" ><div><p>Students about to start their professional journey. Personnels actively looking for job role</p><span><ul><li>🟪</li><li>🟪</li><li>🟪</li><li>🟪</li></ul></span></div></div>
+        <div className="grid3-4"><div><p>Personnels actively looking for job role. Personnels actively looking for job role</p><span><ul><li> 🟧 </li><li>🟧 </li><li>🟧 </li><li>🟧 </li></ul></span></div></div>
+        <div className="grid4-4"><div><p>Personnels actively looking for job role. Personnels actively looking for job role</p><span><ul><li>🟩</li><li>🟩</li><li>🟩</li><li>🟩</li></ul></span></div></div>
+           
+        </div>
+        <div className="header1" style={{position:'absolute', width:'90vw',zIndex:'-1', marginTop:'0vh'}} >
+        <img className="img1" src={bgelement6} style={{maxHeight:'40vh',maxWidth:'40vw',zIndex:'-1',marginTop:'-200px'}}></img>
+        <img className="img3" src={bgelement4} style={{maxHeight:'40vh',maxWidth:'40vw',zIndex:'-1',marginTop:'-200px',marginLeft:'150px'}}></img>
+        <img className="img2" src={bgelement6} style={{maxHeight:'40vh',maxWidth:'40vw',zIndex:'-1',marginTop:'-200px'}}></img>
+        </div> 
+        <div style={{marginTop:'10px'}} className="button"> <button onClick={handleShow} className="bt"><p>Register</p></button></div>
+     
+      <div className="heading" style={{paddingTop: '100px'}}><p>Who Is This Course For?</p></div>
+       <div className="grid">
+        <div >
+          <img className="img4" src={who1} style={{position:'absolute', marginTop:'-80px',marginLeft:'-150px',zIndex:'-1'}}></img> 
+          <div>Anyone who is looking for a job but not sure which job role to take.</div></div>
+        <div >
+           <img className="img5" src={who2} style={{position:'absolute', marginTop:'-130px',marginLeft:'110px',zIndex:'1'}}></img> 
+          <div>Students about to start their professional journey</div></div>
+        <div >
+           <img className="img6" src={who3} style={{position:'absolute', marginTop:'-150px',marginLeft:'80px',zIndex:'1'}}></img> 
+          <div>Personnels actively looking for job role</div></div>
         </div>
     </div>
   );
