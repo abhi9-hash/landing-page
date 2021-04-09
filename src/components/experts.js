dc5981b8b59b154f8d3d48148fff3a62c94dd97b
abhnv_pthk <coolabhinav57@gmail.com>
2021-04-09 23:16:02 +0530

    update expert section

--- src/components/experts.js
+++ src/components/experts.js
@@ -8,7 +8,7 @@ function Experts() {
     return (
         <div>
                   <div className="heading6" id="expert"><p > Meet Our Experts</p></div>
-      <div><img className="img9" src={bgelement5} style={{width:'35%', marginTop:'-35rem', marginBottom:'-15rem',marginLeft:'57%',zIndex:'-1'}}></img> 
+      <div><img className="img9" src={bgelement5} style={{width:'35%', marginTop:'-10rem', marginBottom:'-15rem',marginLeft:'57%',zIndex:'-1'}}></img> 
           </div>
 
      <div className="grid5">
