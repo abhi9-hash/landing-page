1a39c14e0625ba33fcbaab24cf09207d01bfc5e6
abhnv_pthk <coolabhinav57@gmail.com>
2021-04-12 00:53:52 +0530

    updated job section

--- src/components/jobs.js
+++ src/components/jobs.js
@@ -11,13 +11,13 @@ function Jobs() {
 
 <div className="grid1" >
 <div className="grid11">
-    <div className="div">
-        <div  className="divv" ><p >1</p></div>
+    <div className="div" style={{position:'sticky'}}>
+        <div  className="divv"  ><p >1</p></div>
         <img className="howto1" src={howto1}></img>
         </div><div className="div1"><p>Enroll in the Program</p></div></div>
-<div className="grid22" ><div className="div"><div className="divv"><p >2</p></div><img className="howto2" src={howto2}></img></div><div className="div1" ><p>Engage with Our Experts</p></div></div>
-<div className="grid33"><div className="div"><div className="divv"><p >3</p></div><img className="howto3" src={howto3}></img></div><div className="div1"><p>Excel in our Assessments</p></div></div>
-<div className="grid44"><div className="div"><div className="divv"><p >4</p></div><img className="howto4" src={howto4}></img></div><div className="div1"><p>Earn the Job</p></div></div>
+<div className="grid22" ><div className="div" style={{position:'sticky'}}><div className="divv"><p >2</p></div><img className="howto2" src={howto2}></img></div><div className="div1" ><p>Engage with Our Experts</p></div></div>
+<div className="grid33"><div className="div" style={{position:'sticky'}}><div className="divv"><p >3</p></div><img className="howto3" src={howto3}></img></div><div className="div1"><p>Excel in our Assessments</p></div></div>
+<div className="grid44"><div className="div" style={{position:'sticky'}}><div className="divv"><p >4</p></div><img className="howto4" src={howto4}></img></div><div className="div1"><p>Earn the Job</p></div></div>
 </div>
 
             
